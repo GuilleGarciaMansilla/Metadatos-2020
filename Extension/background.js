fb86c9e2635faafac0248c0644db64ed48f6a319
GuilleGarciaMansilla <57759872+GuilleGarciaMansilla@users.noreply.github.com>
2020-04-21 15:52:31 +0200

    Extensión unida a servicios
    Prototipo de fusión con el servicio orquestador y el servicio limpiador de metadatos.

--- Extension/background.js
+++ Extension/background.js
@@ -13,7 +13,7 @@ Orden de ejecucion:
 8.onCompleted
 
 */
-function processB64(b64data){
+function processB64(b64data) {
   var bytes = b64data.split("\n");
   var i = 0;
   var title = "";
@@ -21,66 +21,96 @@ function processB64(b64data){
   var file = "";
   var size = 0;
   bytes.forEach(element => {
-    if(element.includes("title")){
+    if (element.includes("title")) {
       title = bytes[i].split('"')[3];
     }
-    else if(element.includes("content-type")){
+    else if (element.includes("content-type")) {
       contentType = element.split(": ")[1];
     }
-    else if(element.length > size){
+    else if (element.length > size) {
       size = element.length;
       file = element;
     }
     i++;
   });
-  console.log("Title: ",title);
-  console.log("content-type: ",contentType);
-  console.log("file: ",file);
+  // console.log("Title: ", title);
+  // console.log("content-type: ", contentType);
+  // console.log("file: ", file);
   var bin = atob(file);
-  console.log("bin",bin);
+  // console.log("bin", bin);
+  var ret = {
+    'title': title,
+    'type': contentType,
+    'file': file
+  };
+  return ret;
 }
 
 var idRequest;
 var stream;
 //Funcion callback del action listener de onBeforeRequest
-function requestHandlerBody(details){
-  if(details.method == "POST"){
+function requestHandlerBody(details) {
+  if (details.method == "POST") {
     stream = details;
   }
-  
+
 }
 //Funcion callback del action listener de onBeforeSendHeaders
-function requestHandlerHeader(details){
-    
+function requestHandlerHeader(details) {
+
   for (var i = 0; i < details.requestHeaders.length; ++i) {
     //Filtramos las cabeceras POST
-    if(details.method =="POST"){
-       //Filtramos los contenidos de la cabecera por content-type
-      if(details.requestHeaders[i].name.toLowerCase() === 'content-type'){
+    if (details.method == "POST") {
+      //Filtramos los contenidos de la cabecera por content-type
+      if (details.requestHeaders[i].name.toLowerCase() === 'content-type') {
         //Dentro de los content-type solamente queremos los multipart
-        if(details.requestHeaders[i].value.toLowerCase().includes("multipart/related")){
+        if (details.requestHeaders[i].value.toLowerCase().includes("multipart/related")) {
           //Recogemos la id de la peticion para utilizarla en el body
           idRequest = details.requestId;
-          // console.log("Tenemos header ","ID: ",idRequest);
+          console.log("Tenemos header ","ID: ",idRequest);
           // console.log("Header: ",details);
-          if(stream.requestId == idRequest){
-            // console.log("Tenemos body ","ID: ",stream.requestId);
+          if (stream.requestId == idRequest) {
+            console.log("Tenemos body ","ID: ",stream.requestId);
             // console.log("Body: ",stream);
             var enc = new TextDecoder("utf-8");
             var b64data = enc.decode(stream.requestBody.raw[0].bytes);
-            console.log(b64data);
-            processB64(b64data);
-            // console.log(btoa(stream.requestBody.raw[0]));
-            
+            var info = processB64(b64data);
+            //GET FROM DOCKER
+            const url = "http://localhost:8085/formatos";
+            var microservice = "";
+            fetch(url)
+              .then(res => {
+                return res.json();
+              })
+              .then(data => {
+                data.forEach(async element => {
+                  if(info.type.includes(element.nombre)){
+                    microservice = element.url;
+                    try {
+                      //POST TO MS
+                      return fetch(microservice, { 
+                        method: 'POST',
+                        body: info.file
+                      }).then(console.log("Data sent"));
+                    }
+                    catch (error) {
+                      return console.log('Error: ', error);
+                    }
+                    
+                  }
+                });
+              });
+           
+              
           }
         }
-        else if( details.requestHeaders[i].value.toLowerCase().includes("multipart/form-data")){
+        else if (details.requestHeaders[i].value.toLowerCase().includes("multipart/form-data")) {
           idRequest = details.requestId;
-          console.log("Tenemos header ","ID: ",idRequest);
-          console.log("Header: ",details);
-          if(stream.requestId == idRequest){
-            console.log("Tenemos body ","ID: ",stream.requestId);
-            console.log("Body: ",stream);
+          console.log("Tenemos header ", "ID: ", idRequest);
+          // console.log("Header: ", details);
+          if (stream.requestId == idRequest) {
+            console.log("Tenemos body ", "ID: ", stream.requestId);
+            // console.log("Body: ", stream);
             var enc = new TextDecoder("utf-8");
             // console.log(stream.requestBody.raw[1]);
             // filepath = stream.requestBody.raw[1].file;
@@ -93,23 +123,22 @@ function requestHandlerHeader(details){
             //     console.log(elemten.file);
             //   }
             // });
-            console.log("Archivo: ", stream.requestBody.formData.fileToUpload[0]);
           }
         }
       }
     }
-      
+
   }
-  
+
 }
 //Funcion callback del action listener de onHeadersReceived
-function requestHandlerHeaderRcv(details){
+function requestHandlerHeaderRcv(details) {
   //  console.log("Headers:", details);
   for (var i = 0; i < details.responseHeaders.length; ++i) {
-    if( details.responseHeaders[i].name === 'content-type'){
+    if (details.responseHeaders[i].name === 'content-type') {
       console.log(details.responseHeaders[i].value);
     }
-        
+
   }
 }
 
@@ -117,17 +146,17 @@ function requestHandlerHeaderRcv(details){
 // enctype='multipart/form-data'
 function setListeners() {
   chrome.webRequest.onBeforeSendHeaders.addListener(requestHandlerHeader,
-                                                    {urls: ["<all_urls>"]},
-                                                    
-                                                    ["requestHeaders","blocking"]);
+    { urls: ["<all_urls>"] },
+
+    ["requestHeaders", "blocking"]);
   //  chrome.webRequest.onHeadersReceived.addListener(requestHandlerHeaderRcv,
   //                                                   {urls: ["<all_urls>"]},
-                                                    
+
   //                                                   ["responseHeaders","blocking"]);                                              
   chrome.webRequest.onBeforeRequest.addListener(requestHandlerBody,
-                                                {urls: ["<all_urls>"]},
-                                                ['requestBody']);
-   
+    { urls: ["<all_urls>"] },
+    ['requestBody']);
+
   // chrome.webRequest.onCompleted.addListener(requestHandler,
   //                                          {urls: ["<all_urls>"]});
 }
@@ -138,9 +167,9 @@ function removeListeners() {
 }
 //Con esta funcion igual se puede llegar a ver el body a trozos//prototipo!!!!!!!!
 function onBeforeRequestHandler(req) {
-  
+
   if (req.requestBody && req.requestBody.raw) {
-    var requestBody = req.requestBody.raw.map(function(data) {
+    var requestBody = req.requestBody.raw.map(function (data) {
       return decodeURIComponent(String.fromCharCode.apply(null, new Uint8Array(data.bytes)));
     }).join('')
     console.log(requestBody);
